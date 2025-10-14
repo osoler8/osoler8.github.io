6f2d62981c351b27f28e31aa5e3f6efd3e2c8dc5
Oriol <94045341+osoler8@users.noreply.github.com>
2025-10-14 18:52:32 +0200

    Add files via upload

--- generador-dictats/vite.config.ts
+++ generador-dictats/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: base must match the repository name for GitHub Pages
 export default defineConfig({
   plugins: [react()],
   base: '/generador-dictats/',
